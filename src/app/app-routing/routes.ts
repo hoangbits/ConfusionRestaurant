b162b100af5fed7c23fffbfcb1487615c38ae5ff
legiahoang <legiahoang@yahoo.com>
2017-08-08 11:36:14 +0700

    Single Page Applications Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,5 +8,6 @@ import { ContactComponent } from "./../contact/contact.component";
 export const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "menu", component: MenuComponent },
+  { path: "contactus", component: ContactComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Dish } from "./../shared/dist";
+import { Dish } from "./../shared/dish";
 
 @Component({
   selector: "app-dishdetail",
