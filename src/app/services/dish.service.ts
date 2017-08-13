de3b5c6ed1937d8cd4b0cb022fa5e8de7312ac74
legiahoang <legiahoang@yahoo.com>
2017-08-13 15:45:47 +0700

    Promise Part 2-simulate server latency

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -5,12 +5,19 @@ import { DISHES } from "./../shared/dishes";
 export class DishService {
   constructor() {}
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    return new Promise(resolve => {
+      //simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
   getDish(id: number): Promise<Dish> {
-    return Promise.resolve(DISHES.filter(dish => dish.id === id)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter(dish => dish.id === id)[0]), 2000);
+    });
   }
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter(dish => dish.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter(dish => dish.featured)[0]), 2000);
+    });
   }
 }
