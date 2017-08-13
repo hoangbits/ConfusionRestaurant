7c5cd44715e959d4f2b21eafe8bf91645c719352
legiahoang <legiahoang@yahoo.com>
2017-08-13 17:50:16 +0700

    Observable emit one value Promise

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Dish } from "./../shared/dish";
 import { DISHES } from "./../shared/dishes";
+
+import { Observable } from "rxjs/Observable";
+
+import "rxjs/add/operator/toPromise";
+import "rxjs/add/operator/delay";
+import "rxjs/add/observable/of";
+
 @Injectable()
 export class DishService {
   constructor() {}
   getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      //simulate server latency with 2 second delay
-      setTimeout(() => resolve(DISHES), 2000);
-    });
+    // return new Promise(resolve => {
+    //   //simulate server latency with 2 second delay
+    //   setTimeout(() => resolve(DISHES), 2000);
+    // });
+    // return Observable.of(DISHES).delay(2000).toPromise;
+    return Observable.of(DISHES).delay(2000).toPromise();
   }
   getDish(id: number): Promise<Dish> {
     return new Promise(resolve => {
