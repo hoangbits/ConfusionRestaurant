c6806884d8f0b6f06be0ef34c7494fddac258897
legiahoang <legiahoang@yahoo.com>
2017-08-17 13:34:05 +0700

    travis ng serve

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,7 +1,17 @@
-customLaunchers: {
-    // chrome setup for travis CI using chromium
-    Chrome_travis_ci: {
-        base: ‘Chrome’,
-        flags: [‘ — no-sandbox’]
-    }
- },
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular/cli'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-phantomjs-launcher'), /* add PhantomJS launcher here */
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage-istanbul-reporter'),
+        require('@angular/cli/plugins/karma')
+      ],
+      /*...*/
+      browsers: ['Chrome', 'PhantomJS'], /* add PhatomJS here */
+      singleRun: false
+    });
+  };
